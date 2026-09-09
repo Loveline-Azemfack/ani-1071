597bb93b8dac632c88b3bd61b21bbc53f85deaa4
lovelinegatsi <lovelinegatsi@gmail.com>
2026-09-09 14:38:44 +0100

    deuxieme modification

--- chapitre-01/exo1-le_programme_minimal/c1-exo1_main.cpp
+++ chapitre-01/exo1-le_programme_minimal/c1-exo1_main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(){
-    std::cout<<" AZEMFACK GATSI PRINCESSE LOVELINE!  \n"<< std::endl;
+    std::cout<<" AZEMFACK"<< std::endl;
     std::cout << "YAOUNDE" << std::endl;
 
     return 0;
