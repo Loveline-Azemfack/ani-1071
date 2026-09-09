fb54dc1c34b908e684e6d7c5477a92c7b7983c99
lovelinegatsi <lovelinegatsi@gmail.com>
2026-09-09 14:32:40 +0100

    se presenter en deux lignes

---
+++ chapitre-01/exo1-le_programme_minimal/c1-exo1_main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+    std::cout<<"je suis AZEMFACK GATSI PRINCESSE LOVELINE!  \n"<< std::endl;
+    std::cout << "je reside dans la ville de YAOUNDE" << std::endl;
+
+    return 0;
+}
